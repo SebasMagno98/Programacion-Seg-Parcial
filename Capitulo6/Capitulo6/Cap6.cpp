c0a9c950e4ceaa5d6f05a07bc270a16a653bfbbf
USUARIO\PC <idv18c.sgutierrez@uartesdigitales.edu.mx>
2019-02-20 18:06:45 -0600

    Capitulo 6 completo

--- Capitulo6/Capitulo6/Cap6.cpp
+++ Capitulo6/Capitulo6/Cap6.cpp
@@ -3,6 +3,25 @@
 using namespace std;
 
 
+/*
+Sentence:
+	Noun Verb
+	Sentence Conjuction Sentence
+Conjuction:
+	"and"
+	"or"
+	"but"
+	"The"
+Noun:
+	"birds"
+	"fish"
+	"C++"
+Verb:
+	"rules"
+	"fly"
+	"swim"
+*/
+
 const char number = '8';
 class Token
 {
@@ -204,13 +223,187 @@ double expression()
 		}
 	}
 }
-
 //------------------------------------------------------------------------------
 
+void Bulls()
+{
+	std::cout << "En este programa, haremos un pequeño juego de adivinar letras."
+		<< "\nPor cada letra correcta que hayas adivinado se sumara un toro, se ganara si se alcanzo el numero de 4 toros."
+		<< "\nY Por cada letra incorrecta se sumara una vaca, al final del programa se dirán cuantos toros y cuantas vacas se han obtenido.\n";	
+	vector<char>adivinar{'x', 'a' , 'f','g'};
+	int toros = 0;
+	int vacas = 0;
+	char primAdi;
+	char SegAdi;
+	char TerAdi;
+	char CuarAdi;
+	string eleccion;
+	std::cout << "Ingresa el primer valor a adivinar: \n";
+	cin >> primAdi;
+	if (primAdi == adivinar[0])
+	{
+		toros += 1;
+	}
+	else
+	{
+		vacas += 1;
+	}
+	std::cout << "Ingresa el segundo valor a adivinar: \n";
+	cin >> SegAdi;
+	if (SegAdi == adivinar[1])
+	{
+		toros += 1;
+	}
+	else
+	{
+		vacas += 1;
+	}
+	std::cout << "Ingresa el tercer valor a adivinar: \n";
+	cin >> TerAdi;
+	if (TerAdi == adivinar[2])
+	{
+		toros += 1;
+	}
+	else
+	{
+		vacas = +1;
+	}
+	std::cout << "Ingresa el cuarto valor a adivinar: \n";
+	cin >> CuarAdi;
+	if (CuarAdi == adivinar[3])
+	{
+		toros += 1;
+	}
+	else
+	{
+		vacas += 1;
+	}
+	std::cout << "La cantidad de toros conseguidos es de: " << toros << " y la cantidad de vacas conseguidas es de: " << vacas << '\n';
+	if (toros == 4)
+	{
+		std::cout << "Felicidades, acertaste todos los numeros y conseguiste todos los toros. \n";
+	}
+	std::cout << "¿Quieres volver a jugar?"
+		<< "\nIngresa 'Y' para si o 'N' para cerrar.\n";
+	if (eleccion == "Y" or eleccion == "y")
+	{
+		Bulls();
+	}
+	else
+	{
+		std::cout << "Hasta la proxima.\n";
+		exit(-1);
+	}
+	keep_window_open();
+}
+void numeros()
+{
+	std::cout << "En este programa se deben ingresar un maximo de cuatro digitos"
+		<< "\nel programa devolvera las unidades respectivas de esos numeros.";
+	vector<int>numbers;
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+	int opcion;
+	std::cout << "Cuantos valores quieres ingresar?: \n";
+	cin >> opcion;
+	if (opcion == 1) 
+	{
+		std:cout << "Ingresa el numero: \n";
+		cin >> num1;
+		std:cout << "Son: " << num1 << " unidades.\n";
+	}
+	if (opcion == 2)
+	{
+		std::cout << "Ingresa los numeros separados por espacios:\n";
+		cin >> num1, num2;
+		std::cout << "Son: " << num1 << " decenas y " << num2 << " unidades.\n";
+	}
+	if (opcion == 3)
+	{
+		std::cout << "Ingresa los numeros separados por espacios:\n";
+		cin >> num1, num2, num3;
+		std::cout << "Son: " << num1 << " centenas  " << num2 << " decenas " << num3<< " unidades.\n";
+	}
+	if (opcion == 4)
+	{
+		std::cout << "Ingresa los numeros separados por espacios:\n";
+		cin >> num1, num2, num3, num4;
+		std::cout << "Son: " << num1 << " miles  " << num2 << " centenas " << num3 << " decenas" <<num4 << " unidades.\n";
+	}
+}
+
+void Permutacion()
+{
+	std::cout << "En este programa veremos cual es el resultado de una permutacion. \n\n";
+	int a;
+	int b;
+	std::cout << "Ingresa el valor de a: \n";
+	cin >> a;
+	std::cout << "ingresa el valor de b: \n";
+	cin >> b;
+	int resultado = 1;
+	int resultado2 = 1;
+	double perm;
+	int b2 = a - b;
+	{
+		for (int i = 1; i <= a; ++i)
+		{
+			resultado *= i;
+		}
+		a = resultado;
+	}
+	std::cout << "Ingresa el valor de b: \n";
+	{
+		for (int i = 1; i <= b2; ++i)
+		{
+			resultado2 *= i;
+		}
+		b = resultado2;
+	}
+	perm = resultado / resultado2;
+	std::cout << "La permutacion de (a,b) es: " << perm << '\n';
+}
+void combinacion()
+{
+	std::cout << "En este programa veremos cual es el resultado de una combinacion. \n\n";
+	int a;
+	int b;
+	std::cout << "Ingresa el valor de a: \n";
+	cin >> a;
+	std::cout << "ingresa el valor de b: \n";
+	cin >> b;
+	int resultado = 1;
+	int resultado2 = 1;
+	double perm;
+	double combinacion;
+	int b2 = a - b;
+	{
+		for (int i = 1; i <= a; ++i)
+		{
+			resultado *= i;
+		}
+		a = resultado;
+	}
+	std::cout << "Ingresa el valor de b: \n";
+	{
+		for (int i = 1; i <= b2; ++i)
+		{
+			resultado2 *= i;
+		}
+		b = resultado2;
+	}
+	perm = resultado / resultado2;
+	combinacion = perm / b;
+	std::cout << "La combinacion es de: " << combinacion << '\n';
+
+}
 int main()
 {
 	std::cout << "Bienvenido al programa, para poder acceder a la calculadora, ingresa 1."
-		<< "\nPara acceder al Vector e ingresar nombres y edades, ingresa 2. \n";
+		<< "\nPara acceder al Vector e ingresar nombres y edades, ingresa 2. \n"
+		<< "\nY Del 3 al 7 para el resto de ejercicios";
 	int eleccion;
 	cin >> eleccion;
 	if (eleccion == 1)
@@ -277,7 +470,48 @@ int main()
 			std::cout << "\nIngresa un nombre e ingresa una edad separados por un Enter: \n";
 		}
 		keep_window_open();
-		return 0;
-		
+		return 0; 
 	}	
+	if (eleccion == 3)
+	{
+		std::cout << "En este programa revisaremos si la oracion escrita esta bine gramaticamente."
+			<< "\nEl programa devolvera 'OK' en caso de estar bien, y 'Not OK' si es el caso opuesto.\n\n";
+		string Palabras;
+		char oracion[10];
+		std::cout << "Ingresa una oracion: \n";
+		gets_s(oracion);
+		Palabras = oracion;
+		if (Palabras.back() == '.')
+		{
+			std::cout << "OK";
+		}
+		else
+		{
+			std::cout << "Not OK";
+		}
+		keep_window_open();
+		return 0;
+	}
+	if (eleccion == 4)
+	{
+		Bulls();
+		keep_window_open();
+	}
+	if (eleccion == 5)
+	{
+		numeros();
+		keep_window_open();
+	}
+	if (eleccion == 6)
+	{
+		Permutacion();
+		keep_window_open();
+	}
+	if (eleccion == 7)
+	{
+		combinacion();
+		keep_window_open();
+	}
+	keep_window_open();
+	return 0;
 }
